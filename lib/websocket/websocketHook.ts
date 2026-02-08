dae10b370bae363a66fcfdc11c70490339d5912d
varun789-mx <varunsh153@gmail.com>
2026-02-08 11:57:42 +0530

    Change the port to WS_PORT

--- lib/websocket/websocketHook.ts
+++ lib/websocket/websocketHook.ts
@@ -1,12 +1,12 @@
 import { WebsocketManager } from "./server";
 
 let wss: WebsocketManager | null;
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.WS_PORT || 5000;
 export function GetSocket() {
 
     if (!wss) {
         console.log(`Initializing websocket server`);
-
+        console.log(PORT, "current port no ");
         wss = WebsocketManager.getsocket();
         wss.initlisteners();
         wss.listen(Number(PORT));
